69fe73df1ca88655cbe889809a0e6bbc0d03e841
faazah <faizan8t8@gmail.com>
2023-02-22 12:56:39 +0530

    initial commit

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getPosts, createPost, updatePost, deletePost, likePost} from '../controllers.js/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts)
-router.post('/create', createPost);
-router.patch('/update/:id', updatePost);
-router.delete('/delete/:id', deletePost);
-router.patch('/likePost/:id', likePost);
+router.post('/create', auth, createPost);
+router.patch('/update/:id', auth, updatePost);
+router.delete('/delete/:id', auth, deletePost);
+router.patch('/likePost/:id', auth, likePost);
 
-export default router;
+export default router; 
